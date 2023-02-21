2a72596e7e827cd0fc4c0c5bf6ee32740d548eae
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2023-02-21 16:37:51 -0300

    iniciado

---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.bds04.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+
+@Service
+public class CityService {
+	
+	@Autowired
+	private CityRepository repository;
+	
+	public List<CityDTO> findAll() {
+		List<City> list = repository.findAll(Sort.by("name"));
+		return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		repository.save(entity);
+		return new CityDTO(entity);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,41 @@
+package com.devsuperior.bds04.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.CityRepository;
+import com.devsuperior.bds04.repositories.EventRepository;
+
+@Service
+public class EventService {
+	
+	@Autowired
+	private EventRepository repository;
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		Page<Event> list = repository.findAll(pageable);
+		return list.map(x -> new EventDTO(x));
+	}
+	
+	public EventDTO insert(EventDTO dto) {
+		Event entity = new Event();
+		copyDtoToEntity(dto,entity);
+		repository.save(entity);
+		return new EventDTO(entity);
+	}
+	
+	private void copyDtoToEntity(EventDTO dto, Event entity) {
+		entity.setName(dto.getName());
+		entity.setDate(dto.getDate());
+		entity.setUrl(dto.getUrl());
+		entity.setCity(cityRepository.getOne(dto.getCityId()));		
+	}
+
+}
